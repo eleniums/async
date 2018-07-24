d3af26ebe920ce96a66eb6653b15ba2866ef7f8e
Stan Nelson <eleniums@yahoo.com>
2018-07-24 08:31:40 -0700

    Implement functions to process a task and wait for completion
    Needs testing.

--- pool.go
+++ pool.go
@@ -1,6 +1,8 @@
 package async
 
 import (
+	"context"
+
 	"golang.org/x/sync/semaphore"
 )
 
@@ -14,4 +16,32 @@ func NewTaskPool(max int) *TaskPool {
 		max: max,
 		sem: semaphore.NewWeighted(int64(max)),
 	}
+}
+
+func (p *TaskPool) Process(task Task) error {
+	err := p.sem.Acquire(context.Background(), 1)
+	if err != nil {
+		return err
+	}
+
+	go func() {
+		defer p.sem.Release(1)
+		task()
+	}()
+
+	return nil
+}
+
+func (p *TaskPool) Wait() error {
+	for i := 0; i < p.max; i++ {
+		err := p.sem.Acquire(context.Background(), 1)
+		if err != nil {
+			return err
+		}
+	}
+	for i := 0; i < p.max; i++ {
+		p.sem.Release(1)
+	}
+
+	return nil
 }
