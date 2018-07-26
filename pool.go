7a990ebde6d93f7ed745001d96a64f036c6f4546
Stan Nelson <eleniums@yahoo.com>
2018-07-25 18:17:50 -0700

    Add comments

--- pool.go
+++ pool.go
@@ -6,11 +6,13 @@ import (
 	"golang.org/x/sync/semaphore"
 )
 
+// TaskPool limits the number of concurrent tasks being processed to a given max.
 type TaskPool struct {
 	max int
 	sem *semaphore.Weighted
 }
 
+// NewTaskPool creates a new task pool that will limit concurrent tasks to max.
 func NewTaskPool(max int) *TaskPool {
 	return &TaskPool{
 		max: max,
@@ -18,7 +20,8 @@ func NewTaskPool(max int) *TaskPool {
 	}
 }
 
-func (p *TaskPool) Process(task Task) error {
+// Run will block until there is available capacity and then execute the given task.
+func (p *TaskPool) Run(task Task) error {
 	err := p.sem.Acquire(context.Background(), 1)
 	if err != nil {
 		return err
@@ -32,13 +35,17 @@ func (p *TaskPool) Process(task Task) error {
 	return nil
 }
 
+// Wait until all tasks have finished processing.
 func (p *TaskPool) Wait() error {
+	// acquire all available slots in semaphore
 	for i := 0; i < p.max; i++ {
 		err := p.sem.Acquire(context.Background(), 1)
 		if err != nil {
 			return err
 		}
 	}
+
+	// all tasks have completed; release the semaphore
 	for i := 0; i < p.max; i++ {
 		p.sem.Release(1)
 	}
