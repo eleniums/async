ee6ef77ae834e20d08be594307c188d398de807a
Stan Nelson <eleniums@yahoo.com>
2018-07-25 22:01:22 -0700

    Add test for wait

--- pool_test.go
+++ pool_test.go
@@ -106,3 +106,44 @@ func Test_TaskPool_Run_Success(t *testing.T) {
 	assert.True(t, startedTask3)
 	assert.False(t, finishedTask3)
 }
+
+func Test_TaskPool_Wait_Success(t *testing.T) {
+	// arrange
+	startedTask1 := false
+	finishedTask1 := false
+	task1 := func() error {
+		defer func() { finishedTask1 = true }()
+		startedTask1 = true
+		time.Sleep(time.Millisecond * 200)
+		return nil
+	}
+
+	startedTask2 := false
+	finishedTask2 := false
+	task2 := func() error {
+		defer func() { finishedTask2 = true }()
+		startedTask2 = true
+		time.Sleep(time.Millisecond * 200)
+		return nil
+	}
+
+	pool := NewTaskPool(2)
+
+	err := pool.Run(task1)
+	assert.NoError(t, err)
+
+	err = pool.Run(task2)
+	assert.NoError(t, err)
+
+	// act
+	pool.Wait()
+
+	// assert
+	assert.NoError(t, err)
+
+	assert.True(t, startedTask1)
+	assert.True(t, finishedTask1)
+
+	assert.True(t, startedTask2)
+	assert.True(t, finishedTask2)
+}
