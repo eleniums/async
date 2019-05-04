21c12637eca66daee07d5903dbd19370f99886ad
Stan Nelson <eleniums@yahoo.com>
2019-05-04 10:24:34 -0700

    Add pprof scripts

---
+++ scripts/pprof_mem.sh
@@ -0,0 +1,23 @@
+#!/usr/bin/env bash
+set -e
+
+# INSTRUCTIONS:
+# Run benchmark tests first (test_bench.sh) to generate a memory profile.
+# First parameter of this script is the index to use (alloc_space, alloc_objects, inuse_space, or inuse_objects).
+# Alloc indexes track total allocations over time. Inuse tracks current in-memory usage.
+
+# USEFUL PPROF COMMANDS:
+# top - Outputs the top entries (can also do top10 or top20 or topN where N is any number to limit results).
+# list - Shows method code with flat and cum values.
+# web - Visualize graph through web browser (need to `brew install graphviz`).
+# help - List all commands with descriptions.
+
+# cum is total allocations for the method and any methods it calls.
+# flat is total allocations for the method only.
+
+SAMPLE_INDEX=$1
+if [ -z "$SAMPLE_INDEX" ]; then
+    SAMPLE_INDEX=alloc_space
+fi
+
+go tool pprof -sample_index=$SAMPLE_INDEX ./mem.prof
--- scripts/test_bench.sh
+++ scripts/test_bench.sh
@@ -1,4 +1,14 @@
 #!/bin/bash
 set -e
 
-go test -run=^$ -bench=. ./test $@
+if [ -z "$BENCH_TIME" ]
+then
+    export BENCH_TIME=10s
+fi
+
+if [ -z "$BENCH" ]
+then
+    export BENCH=.
+fi
+
+go test -v -run=^$ -bench=$BENCH -benchtime=$BENCH_TIME -memprofile=mem.prof -cpuprofile=cpu.prof ./test $@
